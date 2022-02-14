a7e5f8efb8639e49fc9f3d382793859876de1b44
Piotr Krzaczkowski <piotr.krzaczkowski@embiq.com>
2022-02-15 00:28:10 +0100

    add auth types

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
+import { environment } from 'src/environments/environment';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, StoreModule.forRoot([])],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    StoreModule.forRoot([]),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production, // Restrict extension to log-only mode
+    }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
