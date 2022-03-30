b8dc49a8fd26b05ef6e6afdc36628e3a339a0fb6
Piotr Krzaczkowski <piotr.krzaczkowski@embiq.com>
2022-03-30 16:04:08 +0200

    add feed url

--- src/app/global-feed/components/global-feed/global-feed.component.ts
+++ src/app/global-feed/components/global-feed/global-feed.component.ts
@@ -5,4 +5,6 @@ import { Component } from '@angular/core';
   templateUrl: './global-feed.component.html',
   styleUrls: ['./global-feed.component.scss'],
 })
-export class GlobalFeedComponent {}
+export class GlobalFeedComponent {
+  apiUrl = '/articles';
+}
--- src/app/shared/modules/feed/components/feed/feed.component.ts
+++ src/app/shared/modules/feed/components/feed/feed.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { getFeedAction } from '../../store/actions/getFeed.action';
 
 @Component({
   selector: 'app-feed',
   templateUrl: './feed.component.html',
-  styleUrls: ['./feed.component.scss']
+  styleUrls: ['./feed.component.scss'],
 })
 export class FeedComponent implements OnInit {
+  @Input() apiUrl: string;
 
-  constructor() { }
+  constructor(private store: Store) {}
 
   ngOnInit(): void {
+    console.log('dwa');
+    this.store.dispatch(getFeedAction({ url: this.apiUrl }));
   }
-
 }
