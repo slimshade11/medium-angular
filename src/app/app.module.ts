4cab5989bf1a0b5d58bb4c116b6cbafb47afa3f9
Piotr Krzaczkowski <piotr.krzaczkowski@embiq.com>
2022-03-31 20:55:13 +0200

    add routing-store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { routerReducer, StoreRouterConnectingModule } from '@ngrx/router-store';
 import { NgxLoadingModule, ngxLoadingAnimationTypes } from 'ngx-loading';
 
 import { environment } from 'src/environments/environment';
@@ -13,8 +14,6 @@ import { EffectsModule } from '@ngrx/effects';
 import { TopBarModule } from 'src/app/shared/modules/top-bar/top-bar.module';
 import { AuthFacade } from 'src/app/auth/auth.facade';
 import { AuthInterceptor } from 'src/app/shared/services/auth-interceptor.service';
-import { authReducers } from 'src/app/auth/store/reducers';
-import { feedReducers } from 'src/app/shared/modules/feed/store/reducers';
 import { AuthModule } from 'src/app/auth/auth.module';
 import { GlobalFeedModule } from 'src/app/global-feed/global-feed.module';
 
@@ -32,7 +31,8 @@ import { GlobalFeedModule } from 'src/app/global-feed/global-feed.module';
       secondaryColour: '#ddd',
       tertiaryColour: '#ddd',
     }),
-    StoreModule.forRoot({ auth: authReducers, feed: feedReducers }),
+    StoreModule.forRoot({ router: routerReducer }),
+    StoreRouterConnectingModule.forRoot(),
     StoreDevtoolsModule.instrument({
       maxAge: 25,
       logOnly: environment.production,
--- src/app/shared/modules/feed/store/reducers.ts
+++ src/app/shared/modules/feed/store/reducers.ts
@@ -6,6 +6,7 @@ import {
   getFeedSuccessAction,
   getFeedFailureAction,
 } from 'src/app/shared/modules/feed/store/actions/getFeed.action';
+import { routerNavigationAction } from '@ngrx/router-store';
 
 const initialState: FeedStateInterface = {
   data: null,
@@ -36,7 +37,8 @@ const feedReducer = createReducer(
       ...state,
       isLoading: false,
     })
-  )
+  ),
+  on(routerNavigationAction, (): FeedStateInterface => initialState)
 );
 
 export function feedReducers(state: FeedStateInterface, action: Action) {
