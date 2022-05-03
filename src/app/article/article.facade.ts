549f8063886a6db7fe0862da552cfc89348bb9dc
Piotr Krzaczkowski <piotr.krzaczkowski@embiq.com>
2022-05-03 16:31:37 +0200

    create values for article component

--- src/app/article/article.facade.ts
+++ src/app/article/article.facade.ts
@@ -5,19 +5,26 @@ import { Observable } from 'rxjs';
 
 import { ArticleService } from 'src/app/shared/services/article.service';
 import { ArticleInterface } from 'src/app/shared/types/article.interface';
-import { getArticleAction } from './store/actions/article.actions';
+import { getArticleAction } from 'src/app/article/store/actions/article.actions';
+import { errorSelector, isLoadingSelector, articleSelector } from 'src/app/article/store/selectors';
+import { currentUserSelector } from 'src/app/auth/store/selectors';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ArticleFacade {
-  constructor(private acrticleService: ArticleService, private store: Store) {}
+  error$ = this.store.select(errorSelector);
+  isLoading$ = this.store.select(isLoadingSelector);
+  article$ = this.store.select(articleSelector);
+  currentUser$ = this.store.select(currentUserSelector);
+
+  constructor(private articleService: ArticleService, private store: Store) {}
 
   getArticle(slug: string): Observable<ArticleInterface> {
-    return this.acrticleService.getArticle(slug);
+    return this.articleService.getArticle(slug);
   }
 
   fetchArticle(slug: string): void {
-    this.store.dispatch(getArticleAction({ slug: slug }));
+    this.store.dispatch(getArticleAction({ slug }));
   }
 }
--- src/app/article/article/article.component.ts
+++ src/app/article/article/article.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { combineLatest, map, Observable } from 'rxjs';
 
 import { ArticleFacade } from 'src/app/article/article.facade';
+import { ArticleInterface } from 'src/app/shared/types/article.interface';
+import { CurrentUserInterface } from 'src/app/shared/types/currentUser.interface';
 
 @Component({
   selector: 'app-article',
@@ -8,9 +12,27 @@ import { ArticleFacade } from 'src/app/article/article.facade';
   styleUrls: ['./article.component.scss'],
 })
 export class ArticleComponent implements OnInit {
-  constructor(private articleFacade: ArticleFacade) {}
+  slug: string = this.route.snapshot.paramMap.get('slug');
+
+  article$: Observable<ArticleInterface> = this.articleFacade.article$;
+  error$: Observable<string | null> = this.articleFacade.error$;
+  isLoading$: Observable<boolean> = this.articleFacade.isLoading$;
+  isAuthor$: Observable<boolean> = this.getIsAuthor();
+
+  constructor(private articleFacade: ArticleFacade, private route: ActivatedRoute) {}
 
   ngOnInit(): void {
-    this.articleFacade.fetchArticle('');
+    this.articleFacade.fetchArticle(this.slug);
+  }
+
+  getIsAuthor(): Observable<boolean> {
+    return combineLatest([this.article$, this.articleFacade.currentUser$]).pipe(
+      map(([article, currentUser]: [ArticleInterface | null, CurrentUserInterface | null]) => {
+        if (!article || !currentUser) {
+          return false;
+        }
+        return currentUser.username === article.author.username;
+      })
+    );
   }
 }
